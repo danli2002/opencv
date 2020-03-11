63d471a85bed596e5141ea3693fb30ba8db2b1ae
stezhd <dan.li2010@hotmail.com>
2020-03-10 22:55:30 -0400

    still stuck on the contour search function

--- ball_finder.py
+++ ball_finder.py
@@ -14,11 +14,18 @@ args = vars(ap.parse_args())
 img = cv2.imread(args["input"])
 grayScale = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 blur = cv2.GaussianBlur(grayScale,(101,101),0)
-thresh = cv2.threshold(blur,200,255,cv2.THRESH_BINARY)[1]
+thresh = cv2.threshold(blur,225,255,cv2.THRESH_BINARY)[1]
 
-cv2.imwrite(args["output"],thresh)
+#cv2.imwrite(args["output"],thresh)
 
 
-cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 cnts = imutils.grab_contours(cnts)
 print("There are {} basketballs in this picture".format(len(cnts)))
+
+for c in cnts:
+	cv2.drawContours(thresh,[c],-1,(0,255,255),2)
+	cv2.imshow("Image",thresh)
+	cv2.waitKey(0);
+
+cv2.imwrite(args["output"],thresh)
